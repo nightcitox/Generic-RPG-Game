c0675d58c88e1c508752c6c0429bd3636ae1b3d6
nightcitox <fceaurrutia@gmail.com>
2018-10-15 03:32:53 -0300

    Agregadas las Planillas
    OwO

---
+++ Generic RPG/Assets/Scripts/Objeto.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Objeto : MonoBehaviour {
+    #region Propiedades
+    public PlanObjeto item;
+    private string nombre;
+    private string descripcion;
+    private int nivelRestriccion;
+    private PlanObjeto.Efecto efecto;
+    private PlanObjeto.Tipo tipo;
+    private bool reutilizable;
+    #endregion
+    // Use this for initialization
+    void Start () {
+        nombre = item.nombre;
+        descripcion = item.descripcion;
+        nivelRestriccion = item.nivelRestriccion;
+        efecto = item.efecto;
+        tipo = item.tipo;
+        reutilizable = item.reutilizable;
+	}
+    public void Utilizar()
+    {
+        if(reutilizable == false)
+        {
+            //Elimina el objeto.
+        }
+        else
+        {
+            //Se utiliza
+        }
+    }
+}
---
+++ Generic RPG/Assets/Scripts/Planillas/PlanDialogo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlanDialogo : ScriptableObject {
+    public int id;
+    public string dialogo;
+}
