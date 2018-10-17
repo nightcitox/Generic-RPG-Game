00790c988c10cd47c1cf4ef18d52c914e59b039b
nightcitox <43642659+nightcitox@users.noreply.github.com>
2018-10-17 20:33:50 -0300

    Nuevas Clases Nuevas Fronteras
    Owo

---
+++ Generic RPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    #region Propiedades
+    public PlanMision mision;
+    public string dialogo;
+    #endregion
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
