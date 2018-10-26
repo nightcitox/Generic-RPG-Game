9775c37b24a1a9fdc0079938d0ae1f63fcd1542b
mequieromatar <44039800+mequieromatar@users.noreply.github.com>
2018-10-26 00:16:34 -0300

    Juan Vargas
    Carpeta de los mapas, con habitaciones

---
+++ Generic RPG/Assets/Mapas/2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="2" tilewidth="24" tileheight="22" tilecount="306" columns="17">
+ <image source="LPC MAPS/plant repack.png" width="416" height="416"/>
+</tileset>
---
+++ Generic RPG/Assets/Mapas/Interior3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Interior3" tilewidth="8" tileheight="8" tilecount="1536" columns="32">
+ <image source="LPC MAPS/arabic set/arabic.png" width="256" height="384"/>
+</tileset>
