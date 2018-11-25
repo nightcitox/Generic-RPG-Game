b71bd8b110dab557c8ac1058a28c5d760c5997cb
mequieromatar <44039800+mequieromatar@users.noreply.github.com>
2018-11-25 19:28:30 -0300

    mapas uwu
    jeje

---
+++ Generic RPG/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+	void Update () {
+        float x = GameObject.FindGameObjectWithTag("Jugador").transform.position.x;
+        float y = GameObject.FindGameObjectWithTag("Jugador").transform.position.y;
+        transform.position = new Vector3(x, y, -1);
+    }
+}
